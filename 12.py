483badf79c74f592da1273c24cd92a2e474c7c64
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-04-23 20:17:54 +0400

    Создание словаря.

---
+++ 12.py
@@ -0,0 +1,10 @@
+# Для натурального n создать словарь индекс-значение, 
+# состоящий из элементов последовательности 3n + 1.
+# Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
+
+import os
+ #from typing import Dict
+os.system('cls')
+
+Dictionari = {n:3*n+1 for n in range (1, 7)}
+print (Dictionari)
