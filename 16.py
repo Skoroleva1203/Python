a9f23308ac5690434ad080a934d6e06268ee09e7
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-05-23 11:00:33 +0400

    16. улучшенная версия

--- 16.py
+++ 16.py
@@ -5,11 +5,17 @@ import os
 import random
 os.system('cls')
 
+# Вариант 1
+#n = random.randint(6, 16)
+#print(n)
+#massive = []
+#for i in range (1, n+1):
+#    massive.append(1+(1/i)**i)
+#print(massive)
+#print(f'{sum(massive):.2f}')
+
+# Вариант 2
 n = random.randint(6, 16)
-print(n)
-
-massive = []
-
-for i in range (1, n+1):
-    massive.append(1+(1/i)**i)
-print(f'{sum(massive):.2f}')
+massive = [1+(1/i)**i for i in range(1, n+1)]
+print('Задана последовательность из ', n,
+      'чисел:\n', *massive, '\nСумма чисел = ', round(sum(massive), 4), '\n')
