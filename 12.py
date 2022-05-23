11a8f51d2a27b7b18e45a39adb5398877322fe92
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-05-23 10:59:00 +0400

    12. улучшенная версия

--- 12.py
+++ 12.py
@@ -3,8 +3,12 @@
 # Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
 import os
- #from typing import Dict
 os.system('cls')
 
-Dictionari = {n:3*n+1 for n in range (1, 7)}
-print (Dictionari)
+# Вариант 1
+#Словарь = {n:3*n+1 for n in range (1, 7)}
+#print (Словарь)
+
+# Вариант 2
+print('\nСловарь:', {n: 3 * n + 1 for n in range(1,
+      int(input('Задайте количество элементов последовательности: '))+1)}, '\n')
