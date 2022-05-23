7c6857cc5f6a7b968aa5e7e5bc30ebc5713d32ff
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-05-23 11:00:05 +0400

    15. улучшенная версия

--- 15.py
+++ 15.py
@@ -2,12 +2,18 @@
 # Пример: пусть N = 4, тогда [ 1, 2, 6, 24 ]
 
 import os
-import random
 os.system('cls')
 
+# Вариант 1
+#n = int(input('Введите число N: '))
+#list = [1]
+#for i in range(2, n+1):
+#    list.append(i * list[i-2])
+#print(list, '\n') 
+
+# Вариант 2
+from itertools import accumulate
+import operator
+
 n = int(input('Введите число N: '))
-list = [1]
-    
-for i in range(2, n+1):
-    list.append(i * list[i-2])
-print(list, '\n') 
+print('Получили последовательность:', *list(accumulate([x for x in range(1, n + 1)], operator.mul)), '\n')
