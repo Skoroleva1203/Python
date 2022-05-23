2d8abdaae8092efac125333bfcb7f02d62f27271
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-05-23 10:59:36 +0400

    14. улучшенная версия

--- 14.py
+++ 14.py
@@ -3,13 +3,18 @@ import os
 import random
 os.system('cls')
 
+# Вариант 1
+#a = random.uniform(1, 1001)
+#print('Задано число:', a)
+#a = str(a).replace('.', '')  #переводим в строковый тип и убираем точку
+#print(a)
+#p = list(map(int,a))  # переводим в числовой вид
+#print(p)
+#summa = sum(p) 
+#print('Сумма цифр числа ', a, 'равна:', summa,"\n")
+
+# Вариант 2
 a = random.uniform(1, 1001)
 print('Задано число:', a)
-
-a = str(a).replace('.', '')  #переводим в строковый тип и убираем точку
-print(a)
-
-p = list(map(int,a))  # переводим в числовой вид
-print(p)
-summa = sum(p) 
-print('Сумма цифр числа ', a, 'равна:', summa,"\n")
+print('Сумма цифр числа ', a, ' равна:', 
+    sum(map(int, str(a).replace('.', ''))), '\n')
