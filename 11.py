c63413d82c08e19df79339aaf77125d2bd83a2fc
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-05-23 10:57:50 +0400

    11. улучшенная версия

--- 11.py
+++ 11.py
@@ -1,12 +1,21 @@
 # Сформировать список из  N членов последовательности.
 # Для N = 5: 1, -3, 9, -27, 81 и т.д.
 
-list = [] 
-N = int 
-N = int(input('Введите количество членов последовательности: '))
-for i in range(N):
-    if i% 2 == 0:
-        list.append(3**i)
-    else:
-        list.append(3**i*-1)
+import os
+os.system('cls')
+
+# Вариант 1
+#f1 = lambda x: 3**x
+#f2 = lambda x: 3**x*-1
+#list = [] 
+#N = int(input('Введите количество членов последовательности: '))
+#for i in range(N):
+#    if i% 2 == 0:
+#        list.append(f1(i))
+#    else:
+#        list.append(f2(i))
+#print(list)
+
+# Вариант 2
+list = [(-3) ** num for num in range (int(input('Введите количество элементов: ')))]
 print(list)
